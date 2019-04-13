ed43b34f93814cc9c31c19cba4a712046985503b
Daniel <danielga@outlook.pt>
2019-04-13 22:32:48 +0100

    Merge SpashScreen code with MainActivity on Android

--- Author.Android/MainActivity.cs
+++ Author.Android/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
@@ -6,14 +7,18 @@ namespace Author.Android
 {
     [Activity(Label = "Author",
         Icon = "@drawable/icon",
-        Theme = "@style/MainTheme",
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+        Theme = "@style/Theme.Splash",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar; 
+            ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
