45c2fa9dfb967ad5e8a118ccef5863aaaf17c495
Daniel <danielga@outlook.pt>
2019-04-24 22:03:09 +0100

    Add Clipboard wrapper class

---
+++ Author/Utility/Clipboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Author.Utility
+{
+    public static class Clipboard
+    {
+        public static bool HasText => Xamarin.Essentials.Clipboard.HasText;
+
+        public static Task<string> GetTextAsync() => Xamarin.Essentials.Clipboard.GetTextAsync();
+
+        public static Task SetTextAsync(string text) => Xamarin.Essentials.Clipboard.SetTextAsync(text);
+    }
+}
