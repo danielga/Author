70c4f0c065bc901ed21ebde84c90eb3da2469767
Daniel <danielga@outlook.pt>
2020-01-27 23:55:22 +0000

    WIP

--- Author.UI.Xamarin/UI/EntryManager.cs
+++ Author.UI.Xamarin/UI/EntryManager.cs
@@ -12,100 +12,61 @@ namespace Author.UI
 {
     public class EntryManager
     {
-        public ObservableCollection<MainPageEntryViewModel> Entries { get; } = new ObservableCollection<MainPageEntryViewModel>();
-
+        private readonly ObservableCollection<MainPageEntryViewModel> _entries = new ObservableCollection<MainPageEntryViewModel>();
         private readonly TimeSpan _updateInterval = TimeSpan.FromSeconds(1);
         private readonly HashSet<MainPageEntryViewModel> _visibleEntries = new HashSet<MainPageEntryViewModel>();
         private readonly Database _database = new Database();
         private bool _shouldUpdate = false;
 
+        public ReadOnlyObservableCollection<MainPageEntryViewModel> Entries { get; }
+
         public EntryManager()
         {
+            Entries = new ReadOnlyObservableCollection<MainPageEntryViewModel>(_entries);
+
             Task.Run(async () =>
             {
                 try
                 {
                     await _database.Initialize();
+
+                    List<MainPageEntryViewModel> entries = new List<MainPageEntryViewModel>();
                     foreach (Secret secret in await _database.GetEntries())
                     {
-                        Entries.Add(new MainPageEntryViewModel(secret));
+                        entries.Add(new MainPageEntryViewModel(secret));
                     }
-
-                    Entries.CollectionChanged += OnEntriesChanged;
 
 #if DEBUG
                     // Xamarin.Forms Previewer data
-                    if (Entries.Count != 0)
+                    if (entries.Count == 0)
                     {
-                        return;
-                    }
-
-                    Random random = new Random();
-                    for (int i = 0; i < 5; ++i)
-                    {
-                        MainPageEntryViewModel entry = new MainPageEntryViewModel(new Secret
+                        Random random = new Random();
+                        for (int i = 0; i < 5; ++i)
                         {
-                            Name = "Hello world " + i,
-                            Digits = (byte) (4 + i),
-                            Period = (byte) (5 + i),
-                            Data = new string(Enumerable.Repeat(Base32.ValidCharacters, 32).Select(s => s[random.Next(s.Length)])
-                                .ToArray())
-                        });
-                        Entries.Add(entry);
+                            MainPageEntryViewModel entry = new MainPageEntryViewModel(new Secret
+                            {
+                                Name = "Hello world " + i,
+                                Digits = (byte)(4 + i),
+                                Period = (byte)(5 + i),
+                                Data = new string(Enumerable.Repeat(Base32.ValidCharacters, 32).Select(s => s[random.Next(s.Length)])
+                                    .ToArray())
+                            });
+                            entries.Add(entry);
+                        }
                     }
 #endif
+
+                    await Device.InvokeOnMainThreadAsync(() =>
+                    {
+                        foreach (MainPageEntryViewModel entry in entries)
+                        {
+                            _entries.Add(entry);
+                        }
+                    });
                 }
                 catch
                 {
                     // ignored
-                }
-            });
-        }
-
-        private void OnEntriesChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            Task.Run(async () =>
-            {
-                switch (e.Action)
-                {
-                    case NotifyCollectionChangedAction.Add:
-                        foreach (MainPageEntryViewModel entry in e.NewItems)
-                        {
-                            await _database.AddEntry(entry.Secret);
-                        }
-
-                        break;
-
-                    case NotifyCollectionChangedAction.Remove:
-                        foreach (MainPageEntryViewModel entry in e.OldItems)
-                        {
-                            await _database.RemoveEntry(entry.Secret);
-                        }
-
-                        break;
-
-                    case NotifyCollectionChangedAction.Replace:
-                        foreach (MainPageEntryViewModel entry in e.OldItems)
-                        {
-                            await _database.RemoveEntry(entry.Secret);
-                        }
-
-                        foreach (MainPageEntryViewModel entry in e.NewItems)
-                        {
-                            await _database.AddEntry(entry.Secret);
-                        }
-
-                        break;
-
-                    case NotifyCollectionChangedAction.Move:
-                        break;
-
-                    case NotifyCollectionChangedAction.Reset:
-                        await _database.RemoveEntries();
-                        break;
-
-                    default:
-                        throw new ArgumentOutOfRangeException();
                 }
             });
         }
@@ -157,6 +118,36 @@ namespace Author.UI
             }
 
             _visibleEntries.Remove(entry);
+        }
+
+        public async void Add(MainPageEntryViewModel entry)
+        {
+            await Device.InvokeOnMainThreadAsync(() =>
+            {
+                _entries.Add(entry);
+            });
+
+            await _database.AddEntry(entry.Secret);
+        }
+
+        public async void Remove(MainPageEntryViewModel entry)
+        {
+            await Device.InvokeOnMainThreadAsync(() =>
+            {
+                _entries.Remove(entry);
+            });
+
+            await _database.RemoveEntry(entry.Secret);
+        }
+
+        public async void Reset()
+        {
+            await Device.InvokeOnMainThreadAsync(() =>
+            {
+                _entries.Clear();
+            });
+
+            await _database.RemoveEntries();
         }
     }
 }
