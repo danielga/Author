635dff837f6fef25869f7bcf85077f5e56ea9c0a
Daniel <danielga@outlook.pt>
2021-01-09 19:05:11 +0000

    Update copyright

--- Author.UWP/Properties/AssemblyInfo.cs
+++ Author.UWP/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using Xamarin.Forms.Xaml;
 #endif
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Author")]
-[assembly: AssemblyCopyright("Copyright © 2016-2020 Daniel")]
+[assembly: AssemblyCopyright("Copyright © 2016-2021 Daniel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
